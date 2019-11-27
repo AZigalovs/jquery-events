aae112849708238a01a1701d17682e5445400b6e
AZigalovs <alexzh25@gmail.com>
2019-11-27 21:17:47 +0000

    experiment with toggle effect

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
  
+    
     $("#button_effects1").click(function(){
-        $("#button_effects1").hide("slow");
+        $("#par1").toggle(1000);
+    });
+
+    $("#button_effects2").click(function(){
+        $("#par2").toggle(1000);
+    });
+
+    $("#button_effects3").click(function(){
+        $("#par3").toggle(1000);
     });
 
     $("p").click(function(){
