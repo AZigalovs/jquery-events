ea4fdeb45e6daf23e4c9a5f412f3faa1c3be8027
AZigalovs <alexzh25@gmail.com>
2019-12-01 21:02:42 +0000

    traversing mt DOM

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
  
-    
+    $("p").click(function(){
+        $(this).children("a").css("background-color", "yellow");
+    });
+
+    $("button").click(function(){
+        $(this).prev().slideToggle("slow");
+    });
+
+    $("img").click(function(){
+        $(this).next().children("p").slideDown();
+    });
+
+    $(".card").click(function(){
+        $(this).toggleClass("highlight");
+    });
+
+    $("#select_btn").click(function(){
+        $(".card:not(.highlight)").hide();
+    });
+
+    $("#all_btn").click(function(){
+        $(".card").show();
+    });
+
     $("#button_effects1").click(function(){
         $("#par1").toggle(1000);
     });
